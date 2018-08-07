bedaf6520b551889d105d7717fce78f36bdb3fe9
Kachyz <kachiiz@gmail.com>
2018-08-06 23:59:47 -0500

    Adding password & mail validation

--- backend/app/routes/user.js
+++ backend/app/routes/user.js
@@ -11,13 +11,32 @@ router.post('/signup', (req, res) => {
   const params = req.parameters
   let userParams = params.require('user').permit('email', 'password').value()
 
+  if(!validMail(userParams.email)) {
+    res.send("Wrong email format")
+    return
+  }
+  
+  if(!strongPwd(userParams.password)){
+    res.send("Password is not strong enough")
+    return
+  }
+
   bcrypt.hash(userParams.password, saltRounds, (err, hash) => {
-    console.log(hash);
-    console.log(err);
     userParams.password = hash
     res.send(userParams)
   });
-  
 })
+
+const strongPwd = password => {
+  return (
+    (password.match(/[A-Z]/)) &&
+    (password.match(/[a-z]/) ) &&
+    (password.match(/\d/) ) &&
+    (password.match(/[\s-_!\.@#$%\+\*=]/) ) &&
+    (password.length >= 8)
+  )
+}
+
+const validMail = mail => mail.match(/^[\w\.-]+@\w+\.[a-z]{2,}$/)
 
 module.exports = router
